89f7b80078c0f04df87cb89dbc96bbba41daf500
HiroMuraki <419734782@qq.com>
2026-01-17 17:51:40 +0800

    update

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+from huggingface_hub import snapshot_download
+
+model_sources = [
+    {
+        "repo_id": "black-forest-labs/FLUX.1-dev",
+        "allow_patterns":[
+            "flux1-dev.safetensors",
+            "ae.safetensors"
+        ]
+    },
+    {
+        "repo_id": "comfyanonymous/flux_text_encoders",
+        "allow_patterns":[
+            "clip_l.safetensors",
+            "t5xxl_fp16.safetensors"
+        ]
+    }
+]
+
+print(f"开始下载模型文件...")
+
+os.makedirs("./models", exist_ok=True)
+for model_source in model_sources:
+    snapshot_download(
+        local_dir="./models",
+        **model_source
+    )
+
+print("下载完成！")
